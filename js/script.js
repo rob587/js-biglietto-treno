5a2581295423177531092a48587076244915870f
rob587 <robertocamm02@yahoo.com>
2025-07-01 16:07:14 +0200

    variabili create

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-const travelRequest = prompt("Quanti km vuoi percorrere?")
-const ageRequest = prompt("Quanti anni hai?")
+//su 100km sono 21euro - 20% 16.80 per i minorenni//
+//su 100km sono 21euro - 40% 12.60 per over 65//
+
+const travelRequest =parseInt(prompt("Quanti km vuoi percorrere?"))
+const ageRequest =parseInt(prompt("Quanti anni hai?"))
+
+const yearMinor=18 
+const yearOlder=65
+const ticket= 0.21
+
+
+
+if(ageRequest<yearMinor){
+    console.log(16.80)
+}
+
+else if(ageRequest>yearOlder){
+    console.log(12.60)
+}
+
+else{
+
+}
+
+
